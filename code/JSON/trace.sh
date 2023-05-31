9a6312440e62cf98fbf127c5b74444c3928989e6
Brett David Silverman <brettdavidailverman@gmail.com>
2023-05-31 06:14:08 +0000

    dummy

--- code/JSON/trace.sh
+++ code/JSON/trace.sh
@@ -1,2 +1,2 @@
 rm -f trace.txt
-valgrind  -s --log-file="trace.txt" --track-origins=yes --leak-check=yes ../../build/JSON -test
+valgrind  -s --log-file="trace.txt" --track-origins=yes --leak-check=yes $1 | ../../build/JSON
