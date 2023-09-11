713b2e33b73f59a0328d8970f95e65304846c9bc
Brett David Silverman <brettdavidailverman@gmail.com>
2023-09-11 21:51:53 +1000

    Added streaming of JSON Type

--- code/JSON/JSON.hpp
+++ code/JSON/JSON.hpp
@@ -23,6 +23,52 @@ namespace BeeFishJSON {
       OBJECT
    };
 
+   ostream& operator << (ostream& out, const Type& type)
+   {
+      switch (type)
+      {
+         case Type::UNDEFINED:
+            out << "undefined";
+            break;
+         case Type::_NULL:
+            out << "null";
+            break;
+         case Type::BOOLEAN:
+            out << "bool";
+            break;
+         case Type::NUMBER:
+            out << "number";
+            break;
+         case Type::STRING:
+            out << "string";
+            break;
+         case Type::ARRAY:
+            out << "array";
+            break;
+         case Type::OBJECT:
+            out << "object";
+            break;
+         default:
+            out << "UNKNOWN";
+      }
+
+      return out;
+   }
+
+   PowerEncoding& operator << (PowerEncoding& out, const Type& type)
+   {
+      out << (int)type;
+      return out;
+   }
+
+   PowerEncoding& operator >> (PowerEncoding& in, Type& value)
+   {
+      int type;
+      in >> type;
+      value = (Type)type;
+      return in;
+   }
+
    class JSON : public Parser
    {
    protected:
