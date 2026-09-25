6759c19827f847543a381d6915a8dccf138c4ce5
Brett David Silverman <brettdavidsilverman@gmail.com>
2026-09-25 17:24:23 +1000

    Again with the cors

--- code/https/response-stream.h
+++ code/https/response-stream.h
@@ -161,7 +161,7 @@ public:
                         host +
                         BString("/my")
                     );
-                        
+                    
                 JSONPath myPath =
                     JSONPath::fromString(
                         app->authentication(),
