fdc43d539a94dedaf6df8da9e67dcbdd8f95531b
Brett David Silverman <brettdavidsilverman@gmail.com>
2026-09-25 18:01:33 +1000

    Added debug cors for live

--- code/https/response-stream.h
+++ code/https/response-stream.h
@@ -162,6 +162,11 @@ public:
                         BString("/my")
                     );
                     
+cerr << "HOST " << host << endl;
+cerr << "ORIGIN " << origin << endl;
+cerr << "URL " << url << endl;
+cerr << "MYURL " << myURL << endl;
+                    
                 JSONPath myPath =
                     JSONPath::fromString(
                         app->authentication(),
